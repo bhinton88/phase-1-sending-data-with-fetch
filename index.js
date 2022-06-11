b9804d1abebd1da361cdcec037f12d1c8717cb92
Bobby Hinton <bob.g.hint@gmail.com>
2022-06-11 15:51:39 -0500

    commit tree c50640d287406cf5cd640e3c4edc9e2e7f070c90

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    }),
+  })
+  .then(response => response.json())
+  .then(json => appendID(json))
+  .catch(error => appendError(error))
+}
+
+function appendID (object) {
+  let body = document.querySelector('body');
+  let h1 = document.createElement('h1')
+  h1.textContent = object["id"]
+  body.appendChild(h1)
+}
+
+function appendError (object) {
+  let body = document.querySelector('body');
+  let h1 = document.createElement('h1')
+  h1.textContent = object["message"]
+  body.appendChild(h1)
+}
